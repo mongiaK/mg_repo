c29694e51b1c3c3a6516ccd09cdb12e84b2fafa5
mongia <mongiaK@outlook.com>
2023-07-13 14:21:04 +0800

    alci repo change to mg repo

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm alci_repo*
+rm mg_repo*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R alci_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R mg_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
